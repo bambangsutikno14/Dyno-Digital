3987b6f7b9e269d69aff78c6cdfd2feacd3d60f4
bambangsutikno14 <bambangsutikno14@gmail.com>
2026-04-02 00:22:34 +0700

    Update hiar_tuning_app.py

--- hiar_tuning_app.py
+++ hiar_tuning_app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import pandas as pd
 
 # --- 1. CONFIG & UI ---
-st.set_page_config(Hiar Lima Pendawa Tuning", layout="wide")
+st.set_page_config("Hiar Lima Pendawa Tuning", layout="wide")
 
 st.markdown("""
 <style>
